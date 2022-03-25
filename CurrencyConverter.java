d01fd1976af7537d40cc28ade743ff29432ebd6b
Ubuntu <ubuntu@ip-172-31-90-243.ec2.internal>
2022-03-25 22:16:17 +0000

    Adding changes for recording

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -10,8 +10,9 @@ public class CurrencyConverter {
 
                 DecimalFormat f = new DecimalFormat("##.##");
 		//Adding code to detect if both arguments was passed and none was missed
-		if(args.length == 1) {
-			System.out.println("You have missed to insert two arguments, system requires two arguments: 1st - choose currency 2nd - insert amount to convert. Please re-run the converter and insert both values");
+		if(args.length == 0) {
+
+    throw new IllegalArgumentException("In order to succeed the convertion, you need to pass both arguments. Right now you did pass just 1 or even none. Pleae re-run the converter and pass both: Currency and Amount choosing inputs");
 		}
 
 		//Adding code to check if numbers was passed, not strings or other elements 
@@ -21,7 +22,7 @@ public class CurrencyConverter {
                 }  
                 catch (NumberFormatException e)  
                 { 
-                        System.out.println(args[0] + " is not a valid input, the system accepts only number or double. Please re-run the converter and insert the number"); 
+                        throw new IllegalArgumentException(args[0] + " is not a valid input, the system accepts only number or double. Please re-run the converter and insert the number"); 
                 } 
 
                 try 
@@ -30,7 +31,7 @@ public class CurrencyConverter {
                 }  
                 catch (NumberFormatException e)  
                 { 
-                        System.out.println(args[1] + " is not a valid input, the system accepts only number or double. Pleae re-run the converter and insert the number"); 
+                        throw new IllegalArgumentException(args[1] + " is not a valid input, the system accepts only number or double. Pleae re-run the converter and insert the number"); 
                 } 
 		//If nothing breaks before, making passed arguments from string to double
                 code = Double.parseDouble(args[0]);
