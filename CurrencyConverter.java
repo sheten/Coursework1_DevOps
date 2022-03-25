14081b01fec59427d6e22bece475301bc93eb36a
Ubuntu <ubuntu@ip-172-31-90-243.ec2.internal>
2022-03-25 20:03:04 +0000

    testing sonarqube

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,36 +1,46 @@
+
+
 import java.util.*;
 import java.text.DecimalFormat;
 
 public class CurrencyConverter {
 
-        public main(String[] args) {
+        public static void main(String[] args) {
+		String first, second;
+		first = args[0];
+		second = args[1];
+		handleConvertion(first, second);
+        }
+
+	public static String handleConvertion(String arg1, String arg2) {
 
                 double amount, dollar, pound, code, euro;
 
                 DecimalFormat f = new DecimalFormat("##.##");
-		
 
-		try 
-		{ 
-			Double.parseDouble(args[0]); 
-		}  
-		catch (NumberFormatException e)  
-		{ 
-			System.out.println(args[0] + " is not a valid input, the system accepts only number or double."); 
-		} 
 
-		try 
+
+                try 
                 { 
-                        Double.parseDouble(args[1]); 
+                        Double.parseDouble(arg1); 
                 }  
                 catch (NumberFormatException e)  
                 { 
-                        System.out.println(args[1] + " is not a valid input, the system accepts only number or double."); 
+                        System.out.println(arg1 + " is not a valid input, the system accepts only number or double."); 
+                } 
+
+                try 
+                { 
+                        Double.parseDouble(arg2); 
+                }  
+                catch (NumberFormatException e)  
+                { 
+                        System.out.println(arg2 + " is not a valid input, the system accepts only number or double."); 
                 } 
 
 
-                code = Double.parseDouble(args[0]);
-                amount = Double.parseDouble(args[1]);
+                code = Double.parseDouble(arg1);
+                amount = Double.parseDouble(arg2);
 
                 // For amounts Conversion
                 if (code == 1) {
@@ -40,7 +50,6 @@ public class CurrencyConverter {
 
                         euro = amount * 0.88;
                         System.out.println(amount + " Dollars = " + f.format(euro) + " Euros");
-
                 } else if (code == 2) {
                         // For Pound Conversion
                         dollar = amount + 1.36;
@@ -48,7 +57,6 @@ public class CurrencyConverter {
 
                         euro = amount * 1.19;
                         System.out.println(amount + " Pound = " + f.format(euro) + " Euros");
-
                 } else if (code == 3) {
                         // For Euro Conversion
                         dollar = amount * 1.31;
@@ -56,11 +64,8 @@ public class CurrencyConverter {
 
                         pound = amount * 0.84;
                         System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
-
-
-                }
-	return code;
-        }
-	
+               }
+	return "test";
+	}
 }
 
