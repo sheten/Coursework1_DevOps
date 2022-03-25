8b2ac4606bb8494bf3c5c7309cf7459767a08ebe
Ubuntu <ubuntu@ip-172-31-90-243.ec2.internal>
2022-03-25 22:04:07 +0000

    Added comments

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -9,10 +9,12 @@ public class CurrencyConverter {
                 double amount, dollar, pound, code, euro;
 
                 DecimalFormat f = new DecimalFormat("##.##");
-
+		//Adding code to detect if both arguments was passed and none was missed
 		if(args.length == 1) {
 			System.out.println("You have missed to insert two arguments, system requires two arguments: 1st - choose currency 2nd - insert amount to convert. Please re-run the converter and insert both values");
-}
+		}
+
+		//Adding code to check if numbers was passed, not strings or other elements
                 try 
                 { 
                         Double.parseDouble(args[0]); 
@@ -30,7 +32,7 @@ public class CurrencyConverter {
                 { 
                         System.out.println(args[1] + " is not a valid input, the system accepts only number or double. Pleae re-run the converter and insert the number"); 
                 } 
-
+		//If nothing breaks before, making passed arguments from string to double
                 code = Double.parseDouble(args[0]);
                 amount = Double.parseDouble(args[1]);
 
@@ -57,6 +59,7 @@ public class CurrencyConverter {
                         pound = amount * 0.84;
                         System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
                } else {
+			// Adding code to check that selecting currency was inserted 1,2,3 and if some other value, warning the user
 			System.out.println("Your input choosing currency was: "+code+"  However, the system accepts only numbers: 1,2,3. Please re-run the program and insert the right input");
 		}
 	}
